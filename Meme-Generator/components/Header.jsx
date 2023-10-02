3a9db89f19c546c5c909bbe4282915d0b8191a62
DArlegui <arleguidaniel@gmail.com>
2023-10-01 19:59:24 -0700

    added Header component

---
+++ Meme-Generator/components/Header.jsx
@@ -0,0 +1,8 @@
+export default function Header() {
+  return (
+    <header>
+      <img className="header--img" src="http://www.pngall.com/wp-content/uploads/2016/05/Trollface.png" alt="Problem?" />
+      <h1 className="header--title">Meme Generator</h1>
+    </header>
+  );
+}
--- Meme-Generator/src/App.jsx
+++ Meme-Generator/src/App.jsx
@@ -1,9 +1,10 @@
-import './App.css';
+// import './App.css';
+import Header from '../components/Header';
 
 function App() {
   return (
     <>
-      <p>Test</p>
+      <Header />
     </>
   );
 }
