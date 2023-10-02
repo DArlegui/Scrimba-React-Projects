ae29f49fbc797af0020a214a9c99338f8a6a78f1
DArlegui <arleguidaniel@gmail.com>
2023-10-02 13:36:45 -0700

    Finished Meme component

--- Meme-Generator/components/Header.jsx
+++ Meme-Generator/components/Header.jsx
@@ -1,8 +1,9 @@
 export default function Header() {
   return (
-    <header>
-      <img className="header--img" src="http://www.pngall.com/wp-content/uploads/2016/05/Trollface.png" alt="Problem?" />
-      <h1 className="header--title">Meme Generator</h1>
+    <header className="header">
+      <img className="header--img" src="../images/troll-face.png" alt="Problem?" />
+      <h2 className="header--title">Meme Generator</h2>
+      <h4 className="header--project">React Project 3</h4>
     </header>
   );
 }
---
+++ Meme-Generator/components/Meme.jsx
@@ -0,0 +1,11 @@
+export default function Meme() {
+  return (
+    <main className="meme">
+      <form className="form">
+        <input type="text" placeholder="Top Text" className="form--input" />
+        <input type="text" placeholder="Bottom Text" className="form--input" />
+        <button className="form--button">Get a new meme image 🖼</button>
+      </form>
+    </main>
+  );
+}
