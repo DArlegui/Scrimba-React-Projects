5dd48665a7307a6895aaa20c961d8fbbcf77bb92
DArlegui <arleguidaniel@gmail.com>
2023-10-03 16:22:22 -0700

    Button now works after learning useState hooks

--- Meme-Generator/components/Meme.jsx
+++ Meme-Generator/components/Meme.jsx
@@ -1,11 +1,34 @@
+import React from 'react';
+import memesData from '../src/memesData';
+
 export default function Meme() {
+  const [memeImg, setMeme] = React.useState({
+    topText: '',
+    bottomText: '',
+    randomImg: 'http://i.imgflip.com/1bij.jpg',
+  });
+  const [allMemeImages, setAllMemeImages] = React.useState(memesData);
+
+  function getMemeImage() {
+    const memesArray = allMemeImages.data.memes;
+    const randomNumber = Math.floor(Math.random() * memesArray.length);
+    const url = memesArray[randomNumber].url;
+    setMeme((prevMeme) => ({
+      ...prevMeme,
+      randomImage: url,
+    }));
+  }
+
   return (
     <main className="meme">
-      <form className="form">
+      <div className="form">
         <input type="text" placeholder="Top Text" className="form--input" />
         <input type="text" placeholder="Bottom Text" className="form--input" />
-        <button className="form--button">Get a new meme image 🖼</button>
-      </form>
+        <button className="form--button" onClick={getMemeImage}>
+          Get a new meme image 🖼
+        </button>
+      </div>
+      <img src={memeImg.randomImage} className="meme--image" />
     </main>
   );
 }
